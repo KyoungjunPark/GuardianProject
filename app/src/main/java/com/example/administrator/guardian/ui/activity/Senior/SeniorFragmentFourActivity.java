43c1b1fbda8ce5664c6987b1747ce56d5b717f2f
wonse <dldnjstp20@naver.com>
2016-05-15 20:23:13 +0900

    chart

--- app/src/main/java/com/example/administrator/guardian/ui/activity/Senior/SeniorFragmentFourActivity.java
+++ app/src/main/java/com/example/administrator/guardian/ui/activity/Senior/SeniorFragmentFourActivity.java
@@ -7,8 +7,15 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.annotation.SuppressLint;
 import android.content.Context;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
 
 import com.example.administrator.guardian.R;
+
+import java.util.ArrayList;
 
 /**
  * Created by Administrator on 2016-05-05.
@@ -27,7 +34,35 @@ public class SeniorFragmentFourActivity extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.activity_senior_fragment_four, null);
 
+        LineChart lineChart = (LineChart) view.findViewById(R.id.chart);
+
+        ArrayList<Entry> entries = new ArrayList<>();
+        entries.add(new Entry(4f, 0));
+        entries.add(new Entry(8f, 1));
+        entries.add(new Entry(6f, 2));
+        entries.add(new Entry(2f, 3));
+        entries.add(new Entry(18f, 4));
+        entries.add(new Entry(9f, 5));
+
+        LineDataSet dataset = new LineDataSet(entries, "# of Calls");
+
+        ArrayList<String> labels = new ArrayList<String>();
+        labels.add("January");
+        labels.add("February");
+        labels.add("March");
+        labels.add("April");
+        labels.add("May");
+        labels.add("June");
+
+        LineData data = new LineData(labels, dataset);
+        dataset.setColors(ColorTemplate.COLORFUL_COLORS); //
+        dataset.setDrawCubic(true);
+        dataset.setDrawFilled(true);
+
+        lineChart.setData(data);
+        lineChart.animateY(5000);
 
         return view;
     }
+
 }
