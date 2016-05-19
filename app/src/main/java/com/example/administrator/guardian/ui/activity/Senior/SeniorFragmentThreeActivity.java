5f80f3b2f5e8033fe70d1fa6fef724c0e3567983
KJPARK <parkkj525@naver.com>
2016-05-19 22:57:59 +0900

    update a dialog in visit request

--- app/src/main/java/com/example/administrator/guardian/ui/activity/Senior/SeniorFragmentThreeActivity.java
+++ app/src/main/java/com/example/administrator/guardian/ui/activity/Senior/SeniorFragmentThreeActivity.java
@@ -8,13 +8,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.administrator.guardian.R;
 import com.wdullaer.materialdatetimepicker.date.DatePickerDialog;
 import com.wdullaer.materialdatetimepicker.time.RadialPickerLayout;
 import com.wdullaer.materialdatetimepicker.time.TimePickerDialog;
+import com.yarolegovich.lovelydialog.LovelyStandardDialog;
 
 import java.util.Calendar;
 
@@ -22,8 +23,11 @@ import java.util.Calendar;
 public class SeniorFragmentThreeActivity extends Fragment implements TimePickerDialog.OnTimeSetListener, DatePickerDialog.OnDateSetListener{
 
     private View view;
-    private Button dateTextView;
-    private Button timeTextView;
+    private Button dateButton;
+    private Button timeButton;
+    private Button requestButton;
+    private Button scheduleButton;
+    private EditText infoEditText;
 
     private int year;
     private int month;
@@ -47,10 +51,47 @@ public class SeniorFragmentThreeActivity extends Fragment implements TimePickerD
         view = inflater.inflate(R.layout.activity_senior_fragment_three, null);
 
 
-        dateTextView = (Button) view.findViewById(R.id.dateTextView);
-        timeTextView = (Button) view.findViewById(R.id.timeTextView);
+        dateButton = (Button) view.findViewById(R.id.dateTextView);
+        timeButton = (Button) view.findViewById(R.id.timeTextView);
+        requestButton = (Button) view.findViewById(R.id.requestButton);
+        scheduleButton = (Button) view.findViewById(R.id.scheduleButton);
+        infoEditText = (EditText) view.findViewById(R.id.infoEditText);
 
+        //Set the ClickListener
+        requestButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String info_message;
+                if(hour_of_day > 12) {
+                    info_message = "일시 : " + year+"년 " + month +"월 " + day_of_month + "일 " + "오후" + (hour_of_day-12) + "시 " + minute + "분 "
+                            +"\n" + "내용 : " + infoEditText.getText().toString();
+                } else {
+                    info_message = "일시 : " + year+"년 " + month +"월 " + day_of_month + "일 " + "오전" + hour_of_day + "시 " + minute + "분 "
+                            +"\n" + "내용 : " + infoEditText.getText().toString();
+                }
 
+                new LovelyStandardDialog(getActivity())
+                        .setTopColorRes(R.color.mdtp_light_gray)
+                        .setButtonsColorRes(R.color.mdtp_transparent_black)
+                        .setIcon(R.mipmap.ic_playlist_add_check_black_24dp)
+                        .setTitle(R.string.info_title)
+                        .setMessage(info_message)
+                        .setPositiveButton(android.R.string.ok, new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Toast.makeText(getActivity().getApplicationContext(), "요청 완료", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .setNegativeButton(android.R.string.no, null)
+                        .show();
+            }
+        });
+        scheduleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
         //Set the current date & time
         year = Calendar.getInstance().get(Calendar.YEAR);
         month = Calendar.getInstance().get(Calendar.MONTH);
@@ -58,10 +99,10 @@ public class SeniorFragmentThreeActivity extends Fragment implements TimePickerD
         hour_of_day = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
         minute = Calendar.getInstance().get(Calendar.MINUTE);
 
-        dateTextView.setText(year+"년 " + month +"월 " + day_of_month + "일 ");
+        dateButton.setText(year+"년 " + month +"월 " + day_of_month + "일 ");
         setTime();
         //Set the Date & Time ClickListener
-        dateTextView.setOnClickListener(new View.OnClickListener() {
+        dateButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(getActivity().getApplicationContext(), "HELLO", Toast.LENGTH_LONG);
@@ -75,7 +116,7 @@ public class SeniorFragmentThreeActivity extends Fragment implements TimePickerD
                 dpd.show(getActivity().getFragmentManager(), "Datepickerdialog");
             }
         });
-        timeTextView.setOnClickListener(new View.OnClickListener() {
+        timeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Calendar now = Calendar.getInstance();
@@ -96,7 +137,7 @@ public class SeniorFragmentThreeActivity extends Fragment implements TimePickerD
         this.year = year;
         this.month = monthOfYear;
         this.day_of_month = dayOfMonth;
-        dateTextView.setText(year+"년 " + month +"월 " + day_of_month + "일 ");
+        dateButton.setText(year+"년 " + month +"월 " + day_of_month + "일 ");
     }
 
     @Override
@@ -108,9 +149,9 @@ public class SeniorFragmentThreeActivity extends Fragment implements TimePickerD
     private void setTime()
     {
         if(hour_of_day > 12) {
-            timeTextView.setText("오후" + (hour_of_day-12) + "시 " + minute + "분 ");
+            timeButton.setText("오후" + (hour_of_day-12) + "시 " + minute + "분 ");
         } else {
-            timeTextView.setText("오전" + hour_of_day + "시 " + minute + "분 ");
+            timeButton.setText("오전" + hour_of_day + "시 " + minute + "분 ");
         }
     }
 }
