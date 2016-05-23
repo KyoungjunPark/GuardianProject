5681a893dfe3b490de2a80edf0742d3839d88dbe
wonse <dldnjstp20@naver.com>
2016-05-23 17:01:00 +0900

    ManagerManageInfoMap

--- app/src/main/java/com/example/administrator/guardian/ui/activity/Manager/ManagerManageInfoActivity.java
+++ app/src/main/java/com/example/administrator/guardian/ui/activity/Manager/ManagerManageInfoActivity.java
@@ -14,6 +14,13 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.example.administrator.guardian.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import org.w3c.dom.Text;
 
@@ -30,6 +37,9 @@ public class ManagerManageInfoActivity extends Fragment {
     TextView TextseniorNameAgeGender;
     TextView TextseniorAddress;
     TextView TextseniorNumber;
+
+    private GoogleMap mMap;
+    MapView mapView;
 
     public ManagerManageInfoActivity(Context context, String seniorName, int seniorAge, String seniorGender, String seniorAddress, String seniorNumber){
         mContext=context;
@@ -48,10 +58,31 @@ public class ManagerManageInfoActivity extends Fragment {
         TextseniorNameAgeGender = (TextView)view.findViewById(R.id.TextseniorNameAgeGender);
         TextseniorAddress = (TextView)view.findViewById(R.id.TextseniorAddress);
         TextseniorNumber = (TextView)view.findViewById(R.id.TextseniorNumber);
-        
+
         TextseniorNameAgeGender.setText(seniorName+"("+seniorAge+","+seniorGender+")");
         TextseniorNumber.setText("전화번호 : "+seniorNumber);
         TextseniorAddress.setText("주소 : "+seniorAddress);
+
+        mapView = (MapView)view.findViewById(R.id.vfod_map);
+        mapView.onCreate(new Bundle());
+        mapView.onResume();
+        mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                mMap = googleMap;
+
+                LatLng senior_home = new LatLng( 37.56, 126.97);
+                CameraPosition cameraPosition = new CameraPosition.Builder().target(senior_home).zoom(16).build();
+                mMap.moveCamera( CameraUpdateFactory.newLatLng(senior_home) );
+                MarkerOptions optFirst = new MarkerOptions();
+                optFirst.position(senior_home);// 위도 • 경도
+                optFirst.title(seniorName);// 제목 미리보기
+                optFirst.snippet(seniorAddress+"");
+                mMap.addMarker(optFirst);
+                mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+            }
+        });
+
 
         return view;
     }
