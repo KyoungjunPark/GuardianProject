16d87addffb5c9d68b1dd91f0d37c4dd04a26294
wonse <dldnjstp20@naver.com>
2016-05-24 22:10:26 +0900

    BarChart

--- app/src/main/java/com/example/administrator/guardian/ui/activity/Volunteer/VolunteerFragmentFourActivity.java
+++ app/src/main/java/com/example/administrator/guardian/ui/activity/Volunteer/VolunteerFragmentFourActivity.java
@@ -11,6 +11,12 @@ import android.widget.TextView;
 
 
 import com.example.administrator.guardian.R;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.utils.ColorTemplate;
 
 import org.w3c.dom.Text;
 
@@ -34,6 +40,32 @@ public class VolunteerFragmentFourActivity extends Fragment {
         textVolunteerTime = (TextView)view.findViewById(R.id.textVolunteerTime);
         textVolunteerTime.setText("1"+" : "+"30");
 
+        BarChart BarChart = (BarChart) view.findViewById(R.id.volunteer_time_chart);
+
+        ArrayList<BarEntry> entries = new ArrayList<>();
+        entries.add(new BarEntry(30f, 0));
+        entries.add(new BarEntry(8f, 1));
+        entries.add(new BarEntry(22f, 2));
+        entries.add(new BarEntry(12f, 3));
+        entries.add(new BarEntry(18f, 4));
+        entries.add(new BarEntry(19f, 5));
+
+        BarDataSet dataset = new BarDataSet(entries, "# of Calls");
+
+        ArrayList<String> labels = new ArrayList<String>();
+        labels.add("5/30");
+        labels.add("5/31");
+        labels.add("6/1");
+        labels.add("6/2");
+        labels.add("6/3");
+        labels.add("6/4");
+
+        BarData data = new BarData(labels, dataset);
+        dataset.setColors(ColorTemplate.COLORFUL_COLORS); //
+
+
+        BarChart.setData(data);
+        BarChart.animateY(1000);
 
 
         return view;
