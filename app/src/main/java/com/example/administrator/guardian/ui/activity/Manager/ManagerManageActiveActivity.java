19d3ae7f4543b903b5412950f6302eab0054875e
wonse <dldnjstp20@naver.com>
2016-05-25 18:48:44 +0900

    manager Second Tab

--- app/src/main/java/com/example/administrator/guardian/ui/activity/Manager/ManagerManageActiveActivity.java
+++ app/src/main/java/com/example/administrator/guardian/ui/activity/Manager/ManagerManageActiveActivity.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -23,9 +24,16 @@ import java.util.ArrayList;
 public class ManagerManageActiveActivity extends Fragment {
 
     Context mContext;
-    private int activeDoor=3;
+    private int activeDoor=0;
     private int activeRepri=4;
     private int activeToilet=5;
+    HorizontalBarChart h_BarChart;
+
+    TextView mma_door;
+    TextView mma_doortime;
+    TextView mma_toilettime;
+    TextView mma_repritime;
+    BarData data;
 
     public ManagerManageActiveActivity(Context context){
         mContext=context;
@@ -36,13 +44,26 @@ public class ManagerManageActiveActivity extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.activity_manager_manage_active, null);
 
-        HorizontalBarChart h_BarChart = (HorizontalBarChart) view.findViewById(R.id.M_senior_active_chart);
+        h_BarChart = (HorizontalBarChart) view.findViewById(R.id.M_senior_active_chart);
+        mma_toilettime = (TextView)view.findViewById(R.id.mma_toilettime);
+        mma_repritime=(TextView)view.findViewById(R.id.mma_repritime);
+
+        mma_door = (TextView)view.findViewById(R.id.mma_door);
+        mma_door.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addActiveDoor();
+                Log.d("door",activeDoor+"");
+                h_BarChart.setData(data);
+            }
+        });
+
 
         //심박수 값들
         ArrayList<Integer> activeNumber = new ArrayList<>();
-        activeNumber.add(activeDoor);
-        activeNumber.add(activeRepri);
         activeNumber.add(activeToilet);
+        activeNumber.add(activeRepri);
+        activeNumber.add(activeDoor);
 
         ArrayList<BarEntry> entries = new ArrayList<>();
         entries.add(new BarEntry(activeNumber.get(0), 0));
@@ -52,15 +73,15 @@ public class ManagerManageActiveActivity extends Fragment {
         BarDataSet dataset = new BarDataSet(entries, "# of Calls");
 
         ArrayList<String> labels = new ArrayList<String>();
-        labels.add("출입문");
-        labels.add("냉장고");
         labels.add("화장실");
+        labels.add("냉장고");
+        labels.add("출입문");
 
-        BarData data = new BarData(labels, dataset);
+        data = new BarData(labels, dataset);
         dataset.setColors(ColorTemplate.COLORFUL_COLORS);
 
         h_BarChart.setData(data);
-        h_BarChart.animateY(1000);
+        h_BarChart.animateX(1000);
 
         return view;
     }
